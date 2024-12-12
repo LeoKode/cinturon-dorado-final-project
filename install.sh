a5da1478a6664c9ea61ff4ff63c15e174beb529e
LeoKode <fabbianromero@gmail.com>
2024-12-13 00:47:55 +0100

    Fix JDK installation to use writable directory

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Descargar e instalar Java manualmente
+# Crear un directorio temporal para el JDK en el proyecto
+mkdir -p ./java
+
+# Descargar OpenJDK 11
 curl -L -o openjdk.tar.gz https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
-mkdir -p /opt/java
-tar -xzf openjdk.tar.gz -C /opt/java
+
+# Extraer el JDK al directorio temporal
+tar -xzf openjdk.tar.gz -C ./java --strip-components=1
 
 # Configurar JAVA_HOME y PATH
-export JAVA_HOME=/opt/java/jdk-11.0.2
+export JAVA_HOME=$(pwd)/java
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Verificar la instalación de Java
@@ -14,3 +18,4 @@ java -version
 
 # Compilar el proyecto con Maven Wrapper
 ./mvnw clean package
+
