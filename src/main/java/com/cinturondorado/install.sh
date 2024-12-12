3f2f5435be1a2ef077919bbc6506618d52d05374
LeoKode <fabbianromero@gmail.com>
2024-12-12 15:42:21 +0100

    archivo install.sh

---
+++ src/main/java/com/cinturondorado/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Establece la variable JAVA_HOME (puedes adaptarlo si el entorno ya tiene Java)
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Compila el proyecto con Maven
+./mvnw clean package
